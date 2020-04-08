9889a243a7f540b0cdeb3e93d1b3ddd572ca8ae1
green-green-avk <45503261+green-green-avk@users.noreply.github.com>
2020-04-07 17:46:13 -0700

    Exact signatures match

--- app/src/main/java/green_green_avk/anotherterm/PluginsManager.java
+++ app/src/main/java/green_green_avk/anotherterm/PluginsManager.java
@@ -28,6 +28,7 @@ public final class PluginsManager {
     private PluginsManager() {
     }
 
+    @SuppressLint("PackageManagerGetSignatures")
     public static List<PackageInfo> getPlugins() {
         final PackageManager pm = ctx.getPackageManager();
         final List<PackageInfo> pkgs = pm.getInstalledPackages(0);
@@ -108,7 +109,8 @@ public final class PluginsManager {
     public static boolean verify(@NonNull final PackageInfo pkg) {
         final Set<String> fps = trustedPluginsPrefs.getStringSet(pkg.packageName,
                 Collections.<String>emptySet());
-        if (pkg.signatures == null || pkg.signatures.length <= 0) return false;
+        if (pkg.signatures == null || pkg.signatures.length <= 0
+                || pkg.signatures.length != fps.size()) return false;
         for (final Signature s : pkg.signatures)
             if (!fps.contains(Auth.getFingerprint(s))) return false;
         return true;
