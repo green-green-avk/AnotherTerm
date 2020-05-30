c5e4aedc745e6fbf1899a6915f043e139944a6e3
green-green-avk <45503261+green-green-avk@users.noreply.github.com>
2020-05-30 07:26:53 -0700

    Auto select rules correction

--- app/src/main/java/green_green_avk/anotherterm/utils/CharsAutoSelector.java
+++ app/src/main/java/green_green_avk/anotherterm/utils/CharsAutoSelector.java
@@ -9,7 +9,12 @@ public final class CharsAutoSelector {
 
     private static final byte WORD = 15;
     private static final byte FIELD = 14;
+    private static final byte FIELD_EXT = 13;
+    private static final byte PATH = 12;
     private static final byte SPACE = -1;
+    private static final byte FRAMES = -2;
+
+    private static final byte LAST_GROUP = FRAMES;
 
     private static final byte[] TABLE = new byte[0x200];
 
@@ -24,10 +29,24 @@ public final class CharsAutoSelector {
         TABLE[Character.OTHER_LETTER] = WORD;
         TABLE[Character.CONNECTOR_PUNCTUATION] = WORD;
         TABLE[Character.DASH_PUNCTUATION] = WORD;
-        TABLE['.' | F_ASCII] = FIELD;
+        TABLE[Character.MATH_SYMBOL] = FIELD;
+        TABLE[Character.CURRENCY_SYMBOL] = FIELD;
+        TABLE[Character.OTHER_PUNCTUATION] = FIELD;
+        TABLE['~' | F_ASCII] = FIELD_EXT;
+        TABLE['@' | F_ASCII] = FIELD_EXT;
+        TABLE['!' | F_ASCII] = FIELD_EXT;
+        TABLE['?' | F_ASCII] = FIELD_EXT;
+        TABLE['#' | F_ASCII] = FIELD_EXT;
+        TABLE[Character.START_PUNCTUATION] = FIELD_EXT;
+        TABLE[Character.END_PUNCTUATION] = FIELD_EXT;
+        TABLE[Character.INITIAL_QUOTE_PUNCTUATION] = FIELD_EXT;
+        TABLE[Character.FINAL_QUOTE_PUNCTUATION] = FIELD_EXT;
+        TABLE[Character.MODIFIER_SYMBOL] = FIELD_EXT;
+        TABLE['/' | F_ASCII] = PATH;
         TABLE[Character.SPACE_SEPARATOR] = SPACE;
         TABLE[Character.PARAGRAPH_SEPARATOR] = SPACE;
         TABLE[Character.LINE_SEPARATOR] = SPACE;
+        TABLE[Character.OTHER_SYMBOL] = FRAMES;
     }
 
     private static byte getCat(final char c) {
@@ -44,7 +63,7 @@ public final class CharsAutoSelector {
     public static void select(@NonNull final char[] v, final int start, final int end,
                               final int ptr, @NonNull final int[] ret) {
         final byte cat;
-        if (ptr >= v.length || (cat = getCat(v[ptr])) < 0) {
+        if (ptr >= v.length || (cat = getCat(v[ptr])) <= LAST_GROUP) {
             ret[0] = 0;
             ret[1] = end - 1;
             return;
