013568959150b3e7f6d30fc31e276d450b37b31c
green-green-avk <45503261+green-green-avk@users.noreply.github.com>
2020-11-09 10:05:17 -0800

    Scratchpad polishing...

--- app/src/main/java/green_green_avk/anotherterm/ScratchpadActivity.java
+++ app/src/main/java/green_green_avk/anotherterm/ScratchpadActivity.java
@@ -3,6 +3,8 @@ package green_green_avk.anotherterm;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Point;
+import android.graphics.Rect;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -17,14 +19,13 @@ import androidx.annotation.Keep;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.Date;
 import java.util.List;
 
@@ -55,13 +56,7 @@ public final class ScratchpadActivity extends AppCompatActivity {
             public void run() {
                 list.clear();
                 list.addAll(sm.enumerate());
-                Collections.sort(list, new Comparator<ScratchpadManager.Entry>() {
-                    @Override
-                    public int compare(final ScratchpadManager.Entry o1,
-                                       final ScratchpadManager.Entry o2) {
-                        return (int) (o1.timestamp - o2.timestamp);
-                    }
-                });
+                Collections.sort(list, (o1, o2) -> (int) (o1.timestamp - o2.timestamp));
                 notifyDataSetChanged();
             }
         };
@@ -98,43 +93,64 @@ public final class ScratchpadActivity extends AppCompatActivity {
                                      final int position) {
             if (position >= list.size()) return;
             final ScratchpadManager.Entry entry = list.get(position);
-            holder.itemView.findViewById(R.id.delete).setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(final View v) {
-                    final Context ctx = v.getContext();
-                    UiUtils.confirm(ctx, ctx.getString(R.string.do_you_want_to_delete_this_entry),
-                            new Runnable() {
-                                @Override
-                                public void run() {
-                                    sm.remove(entry.name);
-                                }
-                            });
-                }
+            holder.itemView.findViewById(R.id.delete).setOnClickListener(v -> {
+                final Context ctx = v.getContext();
+                UiUtils.confirm(ctx, ctx.getString(R.string.do_you_want_to_delete_this_entry),
+                        () -> sm.remove(entry.name));
             });
-            holder.itemView.findViewById(R.id.copy).setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(final View v) {
-                    final Uri uri;
-                    try {
-                        uri = sm.getUri(entry.name);
-                    } catch (final FileNotFoundException e) {
-                        return;
-                    }
-                    UiUtils.toClipboard(ScratchpadActivity.this, uri, getDesc(entry.name, 64));
+            holder.itemView.findViewById(R.id.edit).setOnClickListener(v -> {
+                final Uri uri;
+                try {
+                    uri = sm.getUri(entry.name);
+                } catch (final FileNotFoundException e) {
+                    return;
                 }
+                final String type = ScratchpadActivity.this.getContentResolver().getType(uri);
+                final Intent i = new Intent(Intent.ACTION_EDIT)
+                        .setDataAndType(uri, type != null ? type : "text/plain")
+                        .addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION
+                                | Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+                startActivity(i);
             });
-            holder.itemView.findViewById(R.id.share).setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(final View v) {
-                    final Uri uri;
-                    try {
-                        uri = sm.getUri(entry.name);
-                    } catch (final FileNotFoundException e) {
-                        return;
-                    }
-                    UiUtils.share(ScratchpadActivity.this, uri);
+            holder.itemView.findViewById(R.id.view).setOnClickListener(v -> {
+                final Uri uri;
+                try {
+                    uri = sm.getUri(entry.name);
+                } catch (final FileNotFoundException e) {
+                    return;
                 }
+                final String type = ScratchpadActivity.this.getContentResolver().getType(uri);
+                final Intent i = new Intent(Intent.ACTION_VIEW)
+                        .setDataAndType(uri, type != null ? type : "text/plain")
+                        .addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+                startActivity(i);
             });
+            holder.itemView.findViewById(R.id.copy).setOnClickListener(v -> {
+                final Uri uri;
+                try {
+                    uri = sm.getUri(entry.name);
+                } catch (final FileNotFoundException e) {
+                    return;
+                }
+                UiUtils.toClipboard(ScratchpadActivity.this, uri, getDesc(entry.name, 64));
+            });
+            holder.itemView.findViewById(R.id.share).setOnClickListener(v -> {
+                final Uri uri;
+                try {
+                    uri = sm.getUri(entry.name);
+                } catch (final FileNotFoundException e) {
+                    return;
+                }
+                UiUtils.share(ScratchpadActivity.this, uri);
+            });
+            final TextView wName = holder.itemView.findViewById(R.id.name);
+            wName.setOnTouchListener((v, event) -> {
+                if (event.getAction() == MotionEvent.ACTION_DOWN)
+                    if (UiUtils.canScrollHorizontally(wName))
+                        holder.parent.requestDisallowInterceptTouchEvent(true);
+                return false;
+            });
+            wName.setText(entry.name);
             holder.itemView.<TextView>findViewById(R.id.timestamp)
                     .setText(new Date(entry.timestamp).toString());
             holder.itemView.<TextView>findViewById(R.id.size)
@@ -142,16 +158,13 @@ public final class ScratchpadActivity extends AppCompatActivity {
             final TextView wDesc = holder.itemView.findViewById(R.id.description);
             // android:scrollHorizontally does not work
             wDesc.setHorizontallyScrolling(true);
-            wDesc.setOnTouchListener(new View.OnTouchListener() {
-                @Override
-                public boolean onTouch(final View v, final MotionEvent event) {
-                    if (event.getAction() == MotionEvent.ACTION_DOWN)
-                        if (wDesc.canScrollVertically(1) ||
-                                wDesc.canScrollVertically(-1) ||
-                                UiUtils.canScrollHorizontally(wDesc))
-                            holder.parent.requestDisallowInterceptTouchEvent(true);
-                    return false;
-                }
+            wDesc.setOnTouchListener((v, event) -> {
+                if (event.getAction() == MotionEvent.ACTION_DOWN)
+                    if (wDesc.canScrollVertically(1) ||
+                            wDesc.canScrollVertically(-1) ||
+                            UiUtils.canScrollHorizontally(wDesc))
+                        holder.parent.requestDisallowInterceptTouchEvent(true);
+                return false;
             });
             wDesc.setText(getDesc(entry.name, DESCRIPTION_SIZE_LIMIT));
         }
@@ -175,7 +188,25 @@ public final class ScratchpadActivity extends AppCompatActivity {
         this.<TextView>findViewById(R.id.location).setText(sm.locationDesc);
         final RecyclerView wList = findViewById(R.id.list);
         wList.setAdapter(new RecyclerAdapter(sm));
-        wList.setLayoutManager(new LinearLayoutManager(this));
+        final Point sz = new Point();
+        getWindowManager().getDefaultDisplay().getSize(sz);
+        final int cols = Math.max(sz.x / getResources().getDimensionPixelSize(
+                R.dimen.scratchpad_span_width_min), 1);
+        wList.setLayoutManager(new GridLayoutManager(this, cols,
+                RecyclerView.VERTICAL, false));
+        final int sp = getResources().getDimensionPixelSize(R.dimen.field_margin_2x);
+        wList.addItemDecoration(new RecyclerView.ItemDecoration() {
+            @Override
+            public void getItemOffsets(@NonNull final Rect outRect, @NonNull final View view,
+                                       @NonNull final RecyclerView parent,
+                                       @NonNull final RecyclerView.State state) {
+                final int pos = parent.getChildAdapterPosition(view);
+                if (pos < 0)
+                    outRect.setEmpty();
+                else
+                    outRect.set(pos % cols == 0 ? 0 : sp, pos < cols ? 0 : sp, 0, 0);
+            }
+        });
     }
 
     @Override
