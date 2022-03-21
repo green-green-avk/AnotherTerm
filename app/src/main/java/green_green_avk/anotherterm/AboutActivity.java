c56292d73b4eef93bfb867be79dbb5b792a9300e
green-green-avk <45503261+green-green-avk@users.noreply.github.com>
2022-03-20 17:14:40 -0700

    App Info in About

--- app/src/main/java/green_green_avk/anotherterm/AboutActivity.java
+++ app/src/main/java/green_green_avk/anotherterm/AboutActivity.java
@@ -1,6 +1,11 @@
 package green_green_avk.anotherterm;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,5 +20,19 @@ public final class AboutActivity extends AppCompatActivity {
         new AsyncLayoutInflater(this).inflate(R.layout.about_activity,
                 (ViewGroup) getWindow().getDecorView().getRootView(),
                 (view, i, viewGroup) -> setContentView(view));
+    }
+
+    public void onAppInfo(final View view) {
+        try {
+            startActivity(
+                    new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                            .setData(Uri.fromParts(
+                                    "package",
+                                    BuildConfig.APPLICATION_ID,
+                                    null
+                            ))
+            );
+        } catch (final ActivityNotFoundException | SecurityException ignored) {
+        }
     }
 }
