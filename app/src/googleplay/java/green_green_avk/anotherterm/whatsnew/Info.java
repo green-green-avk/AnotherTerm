6fb2c1bdf6dd5ac6f490ae60148b50b7c1377777
green-green-avk <45503261+green-green-avk@users.noreply.github.com>
2022-04-20 13:38:40 -0700

    android:requestLegacyExternalStorage="true" for the 'redist' and 'oldgood' flavors
    https://github.com/green-green-avk/AnotherTerm/issues/19

---
+++ app/src/googleplay/java/green_green_avk/anotherterm/whatsnew/Info.java
@@ -0,0 +1,13 @@
+package green_green_avk.anotherterm.whatsnew;
+
+final class Info {
+    private Info() {
+    }
+
+    static final WhatsNewDialog.Entry[] news;
+
+    static {
+        news = new WhatsNewDialog.Entry[]{
+        };
+    }
+}
