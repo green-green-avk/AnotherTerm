73627084e10b967b19082540ed3c941fbb7feadc
green-green-avk <45503261+green-green-avk@users.noreply.github.com>
2023-01-14 20:20:01 -0800

    Hardware keys mapping editor crash has been fixed

--- app/src/main/java/green_green_avk/anotherterm/ui/DialogUtils.java
+++ app/src/main/java/green_green_avk/anotherterm/ui/DialogUtils.java
@@ -2,10 +2,10 @@ package green_green_avk.anotherterm.ui;
 
 import android.app.Activity;
 import android.app.Dialog;
+import android.view.View;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.DefaultLifecycleObserver;
 import androidx.lifecycle.Lifecycle;
 import androidx.lifecycle.LifecycleOwner;
@@ -16,7 +16,8 @@ public final class DialogUtils {
     private DialogUtils() {
     }
 
-    private static final class DialogLifecycleObserver implements DefaultLifecycleObserver {
+    private static final class DialogLifecycleObserver
+            implements DefaultLifecycleObserver {
         @NonNull
         private final WeakReference<Dialog> dialogRef;
 
@@ -35,13 +36,45 @@ public final class DialogUtils {
     public static void wrapLeakageSafe(@NonNull final Dialog dialog,
                                        @Nullable final Runnable onDismiss) {
         final Activity activity = UiUtils.getActivity(dialog.getContext());
-        if (!(activity instanceof AppCompatActivity))
-            throw new IllegalStateException("Underlying activity is not an AppCompatActivity");
-        final Lifecycle lifecycle = ((AppCompatActivity) activity).getLifecycle();
+        if (!(activity instanceof LifecycleOwner))
+            throw new IllegalArgumentException("Underlying activity is not a LifecycleOwner");
+        final Lifecycle lifecycle = ((LifecycleOwner) activity).getLifecycle();
         final DialogLifecycleObserver observer = new DialogLifecycleObserver(dialog);
         lifecycle.addObserver(observer);
         dialog.setOnDismissListener((_dialog) -> {
             lifecycle.removeObserver(observer);
+            if (onDismiss != null)
+                onDismiss.run();
+        });
+    }
+
+    private static final class DialogViewStateListener
+            implements View.OnAttachStateChangeListener {
+        @NonNull
+        private final WeakReference<Dialog> dialogRef;
+
+        private DialogViewStateListener(@NonNull final Dialog dialog) {
+            dialogRef = new WeakReference<>(dialog);
+        }
+
+        @Override
+        public void onViewAttachedToWindow(@NonNull final View v) {
+        }
+
+        @Override
+        public void onViewDetachedFromWindow(@NonNull final View v) {
+            final Dialog dialog = dialogRef.get();
+            if (dialog != null)
+                dialog.dismiss();
+        }
+    }
+
+    public static void wrapLeakageSafe(@NonNull final View view, @NonNull final Dialog dialog,
+                                       @Nullable final Runnable onDismiss) {
+        final DialogViewStateListener listener = new DialogViewStateListener(dialog);
+        view.addOnAttachStateChangeListener(listener);
+        dialog.setOnDismissListener((_dialog) -> {
+            view.removeOnAttachStateChangeListener(listener);
             if (onDismiss != null)
                 onDismiss.run();
         });
