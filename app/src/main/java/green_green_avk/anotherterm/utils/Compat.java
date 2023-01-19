5ada4fabcf0c5622186aab1d91807cfe22be5ceb
green-green-avk <45503261+green-green-avk@users.noreply.github.com>
2023-01-18 16:20:14 -0800

    Compatibility wrapper for `CharBuffer.subSequence()` has been corrected

--- app/src/main/java/green_green_avk/anotherterm/utils/Compat.java
+++ app/src/main/java/green_green_avk/anotherterm/utils/Compat.java
@@ -11,27 +11,17 @@ public final class Compat {
     }
 
     /**
-     * <a href="https://stackoverflow.com/questions/27460960/bug-when-using-characterbuffers-in-android">https://stackoverflow.com/questions/27460960/bug-when-using-characterbuffers-in-android</a>
-     * but without adding a Java 6 module.
+     * See:
+     * <ul>
+     * <li><a href="https://stackoverflow.com/questions/27460960/bug-when-using-characterbuffers-in-android">https://stackoverflow.com/questions/27460960/bug-when-using-characterbuffers-in-android</a></li>
+     * <li><a href="https://stackoverflow.com/questions/61267495/exception-in-thread-main-java-lang-nosuchmethoderror-java-nio-bytebuffer-flip">https://stackoverflow.com/questions/61267495/exception-in-thread-main-java-lang-nosuchmethoderror-java-nio-bytebuffer-flip</a></li>
+     * </ul>
      */
     @NonNull
     public static CharBuffer subSequence(@NonNull final CharBuffer that,
                                          final int start, final int end) {
-        if (Build.VERSION.SDK_INT >= 19)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT)
             return that.subSequence(start, end);
-        if (that.hasArray()) {
-            if (end > that.remaining())
-                throw new IndexOutOfBoundsException("Bad end");
-            if (start > end)
-                throw new IndexOutOfBoundsException("Bad start");
-            try {
-                return CharBuffer.wrap(that.array(),
-                        start + that.arrayOffset() + that.position(), end - start);
-            } catch (final ArrayIndexOutOfBoundsException e) {
-                throw new IndexOutOfBoundsException(e.getMessage());
-            }
-        }
-        throw new AssertionError(
-                "Unable to handle native and readonly buffers for API < 19");
+        return (CharBuffer) ((CharSequence) that).subSequence(start, end);
     }
 }
