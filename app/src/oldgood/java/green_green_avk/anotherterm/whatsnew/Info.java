65c02f9c26e37988b7e3d3974794bb2de810b034
green-green-avk <45503261+green-green-avk@users.noreply.github.com>
2023-01-30 15:31:46 -0800

    Removing old news

--- app/src/oldgood/java/green_green_avk/anotherterm/whatsnew/Info.java
+++ app/src/oldgood/java/green_green_avk/anotherterm/whatsnew/Info.java
@@ -1,21 +1,8 @@
 package green_green_avk.anotherterm.whatsnew;
 
-import java.util.Date;
-
-import green_green_avk.anotherterm.R;
-
 final class Info {
     private Info() {
     }
 
-    static final WhatsNewDialog.Entry[] news;
-
-    static {
-        news = new WhatsNewDialog.Entry[]{
-                new WhatsNewDialog.Entry(R.string.news_IV_dev20,
-                        Date.UTC(122, 4, 24, 0, 0, 0)),
-                new WhatsNewDialog.Entry(R.string.news_IV_dev24,
-                        Date.UTC(122, 8, 21, 0, 0, 0))
-        };
-    }
+    static final WhatsNewDialog.Entry[] news = new WhatsNewDialog.Entry[]{};
 }
--- app/src/redist/java/green_green_avk/anotherterm/whatsnew/Info.java
+++ app/src/redist/java/green_green_avk/anotherterm/whatsnew/Info.java
@@ -1,25 +1,8 @@
 package green_green_avk.anotherterm.whatsnew;
 
-import java.util.Date;
-
-import green_green_avk.anotherterm.R;
-
 final class Info {
     private Info() {
     }
 
-    static final WhatsNewDialog.Entry[] news;
-
-    static {
-        news = new WhatsNewDialog.Entry[]{
-                new WhatsNewDialog.Entry(R.string.news_IV_dev18,
-                        Date.UTC(122, 3, 20, 0, 0, 0)),
-                new WhatsNewDialog.Entry(R.string.news_IV_dev20,
-                        Date.UTC(122, 4, 24, 0, 0, 0)),
-                new WhatsNewDialog.Entry(R.string.news_IV_dev24,
-                        Date.UTC(122, 8, 21, 0, 0, 0)),
-                new WhatsNewDialog.Entry(R.string.news_IV_dev26,
-                        Date.UTC(122, 9, 25, 0, 0, 0))
-        };
-    }
+    static final WhatsNewDialog.Entry[] news = new WhatsNewDialog.Entry[]{};
 }
