98ff80c21115d6f9dad6850754e8a80a73628ab4
green-green-avk <45503261+green-green-avk@users.noreply.github.com>
2023-01-30 15:31:46 -0800

    Removing old news

--- app/src/oldgood/java/green_green_avk/anotherterm/whatsnew/Info.java
+++ app/src/oldgood/java/green_green_avk/anotherterm/whatsnew/Info.java
@@ -1,21 +1,8 @@
 package green_green_avk.anotherterm.whatsnew;
 
-import java.util.Date;
-
-import green_green_avk.anotherterm.R;
-
 final class Info {
     private Info() {
     }
 
-    static final WhatsNewDialog.Entry[] news;
-
-    static {
-        news = new WhatsNewDialog.Entry[]{
-                new WhatsNewDialog.Entry(R.string.news_IIIv53,
-                        Date.UTC(122, 4, 24, 0, 0, 0)),
-                new WhatsNewDialog.Entry(R.string.news_IIIv57,
-                        Date.UTC(122, 8, 21, 0, 0, 0))
-        };
-    }
+    static final WhatsNewDialog.Entry[] news = new WhatsNewDialog.Entry[]{};
 }
