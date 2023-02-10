79a1d168efceab8471bb59f6464361e6dc22b2f0
green-green-avk <45503261+green-green-avk@users.noreply.github.com>
2023-02-10 11:34:15 -0800

    Better CompoundDrawable

--- app/src/main/java/green_green_avk/anotherterm/ui/drawables/AdvancedScaleDrawable.java
+++ app/src/main/java/green_green_avk/anotherterm/ui/drawables/AdvancedScaleDrawable.java
@@ -5,6 +5,7 @@ import android.graphics.PixelFormat;
 import android.graphics.Rect;
 import android.graphics.Region;
 import android.graphics.drawable.Drawable;
+import android.util.DisplayMetrics;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -12,6 +13,11 @@ import androidx.appcompat.graphics.drawable.DrawableWrapperCompat;
 
 import green_green_avk.anotherterm.ui.UiDimens;
 
+/**
+ * It works as it sounds.
+ * <p>
+ * These compound units looks redundant in some way but for good.
+ */
 public class AdvancedScaleDrawable extends DrawableWrapperCompat {
     public final UiDimens.Length left = new UiDimens.Length(0f,
             UiDimens.Length.Units.PARENT_WIDTH, this::invalidateSelf);
@@ -22,8 +28,16 @@ public class AdvancedScaleDrawable extends DrawableWrapperCompat {
     public final UiDimens.Length bottom = new UiDimens.Length(1f,
             UiDimens.Length.Units.PARENT_HEIGHT, this::invalidateSelf);
 
+    private float density = 1f;
+    private float scaledDensity = density;
+
     public AdvancedScaleDrawable(@Nullable final Drawable drawable) {
         super(drawable);
+    }
+
+    public void setMetrics(@NonNull final DisplayMetrics displayMetrics) {
+        density = displayMetrics.density;
+        scaledDensity = displayMetrics.scaledDensity;
     }
 
     @Override
@@ -32,24 +46,14 @@ public class AdvancedScaleDrawable extends DrawableWrapperCompat {
         invalidateSelf();
     }
 
-    private static int measure(final float length, @NonNull final UiDimens.Length.Units units,
-                               @NonNull final Rect bounds) {
-        switch (units) {
-            case PARENT_WIDTH:
-                return (int) (bounds.width() * length);
-            case PARENT_HEIGHT:
-                return (int) (bounds.height() * length);
-            case PARENT_MIN_DIM:
-                return (int) (Math.min(bounds.width(), bounds.height()) * length);
-            case PARENT_MAX_DIM:
-                return (int) (Math.max(bounds.width(), bounds.height()) * length);
-            default:
-                return 0;
-        }
+    private int measureX(@NonNull final UiDimens.Length length, @NonNull final Rect bounds) {
+        return (int) length.measure(bounds.width(), bounds.height(),
+                density, scaledDensity) + bounds.left;
     }
 
-    private static int measure(@NonNull final UiDimens.Length length, @NonNull final Rect bounds) {
-        return measure(length.getValue(), length.getUnits(), bounds);
+    private int measureY(@NonNull final UiDimens.Length length, @NonNull final Rect bounds) {
+        return (int) length.measure(bounds.width(), bounds.height(),
+                density, scaledDensity) + bounds.top;
     }
 
     @Override
@@ -57,10 +61,10 @@ public class AdvancedScaleDrawable extends DrawableWrapperCompat {
         transparentRegion.set(bounds);
         if (getDrawable() != null) {
             getDrawable().setBounds(
-                    measure(left, bounds),
-                    measure(top, bounds),
-                    measure(right, bounds),
-                    measure(bottom, bounds)
+                    measureX(left, bounds),
+                    measureY(top, bounds),
+                    measureX(right, bounds),
+                    measureY(bottom, bounds)
             );
             transparentRegion.op(getDrawable().getBounds(), Region.Op.DIFFERENCE);
             final Region wtr = getDrawable().getTransparentRegion();
