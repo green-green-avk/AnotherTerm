ab6ca232b4fe901f55a1ecb63d8c7e2703603bdd
green-green-avk <45503261+green-green-avk@users.noreply.github.com>
2023-03-26 18:21:20 -0700

    `ChoreographerCompat` cleanup

--- app/src/main/java/green_green_avk/anotherterm/ui/ChoreographerCompat.java
+++ app/src/main/java/green_green_avk/anotherterm/ui/ChoreographerCompat.java
@@ -9,44 +9,57 @@ import android.view.Choreographer;
 import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 
-public final class ChoreographerCompat {
-    private final Choreographer mChoreographer;
-    private final Handler mHandler;
-
-    {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
-            mChoreographer = Choreographer.getInstance();
-            mHandler = null;
-        } else {
-            mChoreographer = null;
-            mHandler = new Handler(Looper.getMainLooper());
-        }
-    }
-
+public abstract class ChoreographerCompat {
     private ChoreographerCompat() {
     }
 
-    private static final ThreadLocal<ChoreographerCompat> sInstance =
-            new ThreadLocal<ChoreographerCompat>() {
-                @Override
-                protected ChoreographerCompat initialValue() {
-                    return new ChoreographerCompat();
-                }
-            };
+    private static final ThreadLocal<ChoreographerCompat> threadInstance =
+            Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN ?
+                    new ThreadLocal<ChoreographerCompat>() {
+                        @Override
+                        @NonNull
+                        protected ChoreographerCompat initialValue() {
+                            return new ChoreographerCompatImplNew();
+                        }
+                    } :
+                    new ThreadLocal<ChoreographerCompat>() {
+                        @Override
+                        @NonNull
+                        protected ChoreographerCompat initialValue() {
+                            return new ChoreographerCompatImplOld();
+                        }
+                    };
 
+    @NonNull
     public static ChoreographerCompat getInstance() {
-        return sInstance.get();
+        return threadInstance.get();
     }
 
-    public void post(@NonNull final Runnable runnable) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN)
-            _post(runnable);
-        else
-            mHandler.postDelayed(runnable, ValueAnimator.getFrameDelay());
+    public abstract void post(@NonNull final Runnable runnable);
+
+    private static final class ChoreographerCompatImplOld extends ChoreographerCompat {
+        private final Handler handler;
+
+        {
+            final Looper looper = Looper.myLooper();
+            if (looper == null)
+                throw new IllegalStateException("The current thread must have a looper!");
+            handler = new Handler(looper);
+        }
+
+        @Override
+        public void post(@NonNull final Runnable runnable) {
+            handler.postDelayed(runnable, ValueAnimator.getFrameDelay());
+        }
     }
 
     @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
-    private void _post(@NonNull final Runnable runnable) {
-        mChoreographer.postFrameCallback(frameTimeNanos -> runnable.run());
+    private static final class ChoreographerCompatImplNew extends ChoreographerCompat {
+        private final Choreographer choreographer = Choreographer.getInstance();
+
+        @Override
+        public void post(@NonNull final Runnable runnable) {
+            choreographer.postFrameCallback(frameTimeNanos -> runnable.run());
+        }
     }
 }
