edb4d2e5e4e885b73a8bfff454f378de950b6212
green-green-avk <45503261+green-green-avk@users.noreply.github.com>
2023-04-30 15:10:12 -0700

    https://issuetracker.google.com/issues/37060141 seems fixed in Oreo...

--- UsbSerial/src/main/java/com/felhr/utils/SafeUsbRequest.java
+++ UsbSerial/src/main/java/com/felhr/utils/SafeUsbRequest.java
@@ -1,29 +1,26 @@
 package com.felhr.utils;
 
 import android.hardware.usb.UsbRequest;
+import android.os.Build;
 
 import java.lang.reflect.Field;
 import java.nio.ByteBuffer;
 
 public class SafeUsbRequest extends UsbRequest {
-    static final String usbRqBufferField = "mBuffer";
-    static final String usbRqLengthField = "mLength";
-
     @Override
     public boolean queue(final ByteBuffer buffer, final int length) {
-        final Field usbRequestBuffer;
-        final Field usbRequestLength;
-        try {
-            usbRequestBuffer = UsbRequest.class.getDeclaredField(usbRqBufferField);
-            usbRequestLength = UsbRequest.class.getDeclaredField(usbRqLengthField);
-            usbRequestBuffer.setAccessible(true);
-            usbRequestLength.setAccessible(true);
-            usbRequestBuffer.set(this, buffer);
-            usbRequestLength.set(this, length);
-        } catch (final Exception e) {
-            throw new RuntimeException(e);
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O) {
+            try {
+                final Field usbRequestBuffer = UsbRequest.class.getDeclaredField("mBuffer");
+                final Field usbRequestLength = UsbRequest.class.getDeclaredField("mLength");
+                usbRequestBuffer.setAccessible(true);
+                usbRequestLength.setAccessible(true);
+                usbRequestBuffer.set(this, buffer);
+                usbRequestLength.set(this, length);
+            } catch (final Exception e) {
+                throw new RuntimeException(e);
+            }
         }
-
         return super.queue(buffer, length);
     }
 }
